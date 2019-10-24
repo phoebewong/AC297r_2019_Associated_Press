1f777e52b7dd979367b0426ff0879cf899fa8f1d
Phoebe Wong <phoebe.wong@berkeley.edu>
2019-10-24 17:11:17 -0400

    add textrank get named entities only function

--- src/nlp_util/textacy_util.py
+++ src/nlp_util/textacy_util.py
@@ -1,6 +1,7 @@
 import textacy
 import pandas as pd
-
+from spacy.tokens import Doc
+import numpy as np
 
 def get_textacy_name_entities(text, article_id, drop_determiners=True, exclude_types='numeric'):
     '''Get Named Entities using textacy
@@ -13,6 +14,8 @@ def get_textacy_name_entities(text, article_id, drop_determiners=True, exclude_t
     en = textacy.load_spacy_lang("en_core_web_sm", disable=("parser",))
     if isinstance(text, str):
         doc = textacy.make_spacy_doc(text, lang=en)
+    elif isinstance(text, Doc): # if pre-created spacy doc
+        doc = doc
     else:
         doc = textacy.make_spacy_doc("NA", lang=en)
 
@@ -29,3 +32,30 @@ def get_textacy_name_entities(text, article_id, drop_determiners=True, exclude_t
     data = data.drop_duplicates(keep='first')
     data['article_id'] = article_id
     return data
+
+def get_textrank_entities_only(textrank_words, entities_list, return_count=False):
+    '''
+    Return textrank results with only named entities
+    Parameters:
+    textrank_words: numpy array of textrank ngram tokens
+    entities_list: list or numpy array of named entities extracted
+
+    Return:
+    A numpy array of textrank ngram tokens that contain named entities extracted
+    '''
+    ne_count = []
+
+    for textrank in textrank_words: # for each textrank ngram token
+        temp_score = 0
+        for entities in entities_list: # for each named entities extracted
+            if (entities in textrank):
+                temp_score +=1
+            else:
+                temp_score = temp_score
+        # Counts of named entities in each text rank word
+        ne_count.append(temp_score)
+
+    if return_count:
+        return textrank_words[np.array(ne_count) > 0], counts
+    else:
+        return textrank_words[np.array(ne_count) > 0]
