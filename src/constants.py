b1069f64919e43c89b2cca3ba4f884365ade16db
kareenaaahuang <qhuang@g.harvard.edu>
2019-11-01 15:51:15 -0400

    added training / test data folder

--- src/constants.py
+++ src/constants.py
@@ -12,6 +12,16 @@ OUTPUT_CSV_DIR = PROJECT_ROOT / 'data' / 'csv_outputs'
 TAXONOMY_DIR = PROJECT_ROOT / 'data' / 'taxonomy_data'
 CLEAN_DIR = PROJECT_ROOT / 'data' / 'clean_data'
 HIST_DIR = PROJECT_ROOT / 'data' / 'clean_data' / 'clean_history'
+TRAIN_TEST_DIR = PROJECT_ROOT / 'data' / 'train_test_data'
+TRAIN_DIR = PROJECT_ROOT / 'data' / 'train_test_data'/'train'
+TEST_DIR = PROJECT_ROOT / 'data' / 'train_test_data'/'test'
+VAL_DIR = PROJECT_ROOT / 'data' / 'train_test_data'/'val'
+
+
+#train test split params
+data_split_random_state = 8
+test_size = 0.2
+val_size = 0.1
 
 # folder names
 ARTICLE_DIR_NAME = 'article'
@@ -22,10 +32,11 @@ FULL_TEXT_DIR_NAME = 'full_text'
 Text_Prefix = 'article_'
 Media_Prefix = 'image_'
 
-# removal history file name
-media_removal_hist_name = 'removed_media.txt'
-article_removal_hist_name = 'removed_article.txt'
-
 # txt files
 DATA_TRUTH = 'HarvardCapstone.txt'
 IMAGE_DUPLICATES_FILENAME = DATA_DIR / 'image_duplicates.txt'
+media_removal_hist_name = 'removed_media.txt'
+article_removal_hist_name = 'removed_article.txt'
+train_aid_filename = 'train_aid.txt'
+test_aid_filename = 'test_aid.txt'
+val_aid_filename = 'val_aid.txt'
