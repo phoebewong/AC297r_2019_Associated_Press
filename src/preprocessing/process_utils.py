8b287865f116e299e8543875bd7802d1ede6f2e1
kareenaaahuang <qhuang@g.harvard.edu>
2019-11-03 17:19:58 -0500

    updated helper function for preprocessing

--- src/preprocessing/process_utils.py
+++ src/preprocessing/process_utils.py
@@ -7,8 +7,6 @@ from collections import defaultdict, Counter
 #directory path
 train_directory = constants.TRAIN_DIR
 
-test_file = pd.read_csv(f'{train_directory}/article_summary.csv')
-
 def remove_char(text_string):
     "remove all characters in given text string"
     new_str = re.sub('[\W_]+', ' ', text_string)
@@ -21,18 +19,3 @@ def get_sentence(text_string):
     while '' in svec:
         svec.remove('')
     return svec
-
-print(get_sentence(test_file.summary.values[0]))
-
-# def format_text(text_string):
-#
-#
-#
-# def get_word_dictionaries(train_data_path = train_directory):
-#     "get dictionaries for tokenization given training data"
-
-# for i in range(len(test_file.summary.values)):
-#     try:
-#         print(split_sentences(test_file.summary.values[i])[-1])
-#     except:
-#         continue
