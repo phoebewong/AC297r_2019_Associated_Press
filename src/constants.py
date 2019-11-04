dfbe5bb75ccd8fc39bc7e368997d6aba526b980b
kareenaaahuang <qhuang@g.harvard.edu>
2019-11-04 14:52:13 -0500

    path added to access stanford parser

--- src/constants.py
+++ src/constants.py
@@ -16,7 +16,7 @@ TRAIN_TEST_DIR = PROJECT_ROOT / 'data' / 'train_test_data'
 TRAIN_DIR = PROJECT_ROOT / 'data' / 'train_test_data'/'train'
 TEST_DIR = PROJECT_ROOT / 'data' / 'train_test_data'/'test'
 VAL_DIR = PROJECT_ROOT / 'data' / 'train_test_data'/'val'
-
+STANFORD_PARSER = PROJECT_ROOT / 'stanford-parser-full-2018-10-17'
 
 #train test split params
 data_split_random_state = 8
