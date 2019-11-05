503a2a2e84520d382cee85b0af0d0974f4d2416e
kareenaaahuang <qhuang@g.harvard.edu>
2019-11-05 13:44:28 -0500

    modified helper function for tag importance measurement

--- src/preprocessing/parse_grammar.py
+++ src/preprocessing/parse_grammar.py
@@ -1,8 +1,12 @@
 #dependencies
 import os
+import pandas as pd
+import numpy as np
 from src import constants
 from nltk.tree import Tree, ParentedTree
 from nltk.parse import stanford
+from nltk.corpus import wordnet as wn
+import process_utils
 
 #set environment variables to access stanford parser
 os.environ['CLASSPATH'] = str(constants.STANFORD_PARSER)
@@ -11,17 +15,46 @@ os.environ['STANFORD_MODELS'] = str(constants.STANFORD_PARSER)
 #initiate stanford parser
 parser = stanford.StanfordParser()
 
-def if_pp(sentence, tag):
-    "check whether scene tag exists as prepositional phrase in sentence"
-    parsed_sentence = parser.raw_parse((sentence))
+def pp(sentences):
+    '''
+    retrieve list of prepositional phrases in tree.
+
+    Params:
+    -------
+    1) sentences: list or array-like, sentences to process
+
+    Return:
+    -------
+    list of prepositional phrases
+    '''
+    parsed_sentence = parser.raw_parse_sents((sentences))
     #cache list to record all prepositional phrases
     pp_phrases = list()
     for object in parsed_sentence:
         for sentence in object:
+            pp = list()
             #retrieve all subtrees starting with PP
             for i in sentence.subtrees(filter = lambda x: x.label() == 'PP'):
-                pp_phrases.extend(i.leaves())
-    if tag in pp_phrases:
-        return True
-    else:
-        return False
+                pp.extend(i.leaves())
+            pp_phrases.append(pp)
+    return pp_phrases
+
+def get_scene_factors(pps, tags):
+    "get scene factor given prepositional phrases and target scene tags"
+    sfs = [1 if tags[i] in pps[i] else 2 for i in range(len(tags))]
+    return np.array(sfs)
+
+def get_synonyms(phrase):
+    "get set of synonyms using wordnet"
+    if ' ' in phrase:
+        #concatenate phrases if applies
+        phrase = phrase.replace(' ', '_')
+    syns = list()
+    syns.append(phrase)
+    synsets = wn.synsets(phrase)
+    for s in synsets:
+        if '.n.' in s.name():
+            syns.extend(s.lemma_names())
+    unique = list(set(syns))
+    unique = [x.replace('_', ' ').lower() for x in unique]
+    return unique
