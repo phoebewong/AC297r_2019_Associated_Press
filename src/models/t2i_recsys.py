92eecc5519842b76b41dc58d936dbae5614216df
kareenaaahuang <qhuang@g.harvard.edu>
2019-11-13 16:54:15 -0500

    updated cosine similarity recommendations

--- src/models/t2i_recsys.py
+++ src/models/t2i_recsys.py
@@ -2,18 +2,25 @@ import json
 import numpy as np
 import pandas as pd
 from src import constants
-from scipy.sparse import load_npz
+from scipy.sparse import load_npz, csr_matrix
+from sklearn.metrics.pairwise import cosine_similarity
 
 #directory path
 data_directory = constants.DATA_DIR
 clean_directory = constants.CLEAN_DIR
 #update this in constants.py
 imp_matrix_filename = 'imp_matrix.npz'
+#load presaved matrix and switch back to dense matrix
+img_imp_matrix_sparse = load_npz(f'{data_directory}/{imp_matrix_filename}')
+img_imp_matrix = img_imp_matrix_sparse.todense()
 #save filename
 tag_idx_filename = 'tag_idx.json'
 img_idx_filename = 'img_idx.json'
-#load presaved matrix and switch back to dense matrix
-img_imp_matrix = load_npz(f'{data_directory}/{imp_matrix_filename}').todense()
+#load index dictionaries
+with open(f'{data_directory}/{tag_idx_filename}', 'r') as tag_idx_file, \
+     open(f'{data_directory}/{img_idx_filename}', 'r') as img_idx_file:
+     tag_idx_dict = json.load(tag_idx_file)
+     img_idx_dict = json.load(img_idx_file)
 
 class T2I:
     "class function for tag-to-image recommendation"
@@ -21,6 +28,7 @@ class T2I:
         self.idx = art_id #article id
         self.ranked_tags = ranked_tags #tags used in recommendation search
         self.ranked_imp = ranked_imp #tag importance score by textrank
+        self.img_imp_matrix_sparse = img_imp_matrix_sparse
         self.img_imp_matrix = img_imp_matrix
         self.tag_vec_shape = self.img_imp_matrix.shape[1]
         self.img_idx_omit = None #record already associated image ids
@@ -40,20 +48,8 @@ class T2I:
             #replace with formated tags
             self.ranked_tags[i] = t.lower().replace(',', '')
 
-    def predict(self, output_size, method = 'dot_product', remove_original = True):
-        "recommend images based on given method"
-        #load index dictionaries
-        with open(f'{data_directory}/{tag_idx_filename}', 'r') as tag_idx_file, \
-             open(f'{data_directory}/{img_idx_filename}', 'r') as img_idx_file:
-             tag_idx_dict = json.load(tag_idx_file)
-             img_idx_dict = json.load(img_idx_file)
-
-        #format tags
-        self.format_tags()
-        if self.img_idx_omit is None:
-            #get associated images
-            self.get_associated_images()
-
+    def prep_article_imp_vec(self):
+        "get article importance vector for recommendation"
         #cache article ranked importance vector
         art_imp_vec = np.zeros(self.tag_vec_shape)
 
@@ -65,12 +61,28 @@ class T2I:
             #update article ranked importance vector
             art_imp_vec[t_idx] = score
 
+        return art_imp_vec
+
+    def predict(self, output_size, method = 'dot_product', remove_original = True):
+        "recommend images based on given method"
+        #format tags
+        self.format_tags()
+        if self.img_idx_omit is None:
+            #get associated images
+            self.get_associated_images()
+
+        #get article ranked importance vector
+        art_imp_vec = self.prep_article_imp_vec()
+
         if method == 'dot_product':
+            img_scores = dot_product(self.img_imp_matrix, art_imp_vec)
+
+        elif method == 'cosine_distance':
             #compute dot_product
-            img_scores = np.asarray(np.dot(self.img_imp_matrix, art_imp_vec.T))
-            flatten_img_scores = img_scores.flatten()
-            #sort for desired number of output
-            output_idx = (-flatten_img_scores).argsort()
+            img_scores = cosine_distance(self.img_imp_matrix_sparse, art_imp_vec)
+
+        #sort for desired number of output
+        output_idx = (-img_scores).argsort()
 
         #get output image ids
         output_img_ids = list()
@@ -82,3 +94,25 @@ class T2I:
                     break
 
         return output_img_ids
+
+def dot_product(ref_matrix, comp_vec):
+    '''
+    Compute dot product distances between input article
+    tag vector and image importance matrix.
+    '''
+    #compute dot_product
+    img_scores = np.asarray(np.dot(ref_matrix, comp_vec.T)).flatten()
+    return img_scores
+
+def cosine_distance(ref_matrix, comp_vec):
+    '''
+    compute cosine distances between input article
+    tag vector and image importance matrix.
+    '''
+    #reshape vector
+    comp_vec_new = np.repeat(comp_vec.reshape(1,-1), ref_matrix.shape[0], axis = 0)
+    #turn to sparse matrix 
+    comp_vec_new = csr_matrix(comp_vec_new)
+    #compute cosine distance
+    img_scores = cosine_similarity(comp_vec_new, ref_matrix)
+    return img_scores[0]
